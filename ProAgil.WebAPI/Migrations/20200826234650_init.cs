929894c2109ee3c14498b2acad159b746d93fc2e
Mafra <rgmafra@hotmail.com>
2020-08-27 21:57:41 -0300

    asd

---
+++ ProAgil.WebAPI/Migrations/20200826234650_init.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagemURL",
+                table: "Eventos",
+                newName: "ImagemUrl");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagemUrl",
+                table: "Eventos",
+                newName: "ImagemURL");
+        }
+    }
+}
